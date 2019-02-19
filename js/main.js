cb3a92c66e4eef7dc16a98301abc1022169da817
Jonathan Azali <azali.jonathan@gmail.com>
2019-02-19 09:52:24 -0800

    toggle is active

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import toggleActive from "./toggleActive.js";
+
+
+
+// add event listener (Click) to each buttons
+// select edit button
+document.querySelectorAll('.edit-button').forEach(button => button.addEventListener('click', toggleActive));
+
---
+++ js/toggleActive.js
@@ -0,0 +1,14 @@
+
+/**
+ * Toggles  active class on editable text
+ * @param {Event} event 
+ */
+
+// function that will add a class to editable text
+export default function toggleActive(event) {
+    //get the parent element
+    const editableText = event.target.parentElement;
+    console.log(editableText);
+    //toggle the class active (on some element)
+    editableText.classList.toggle('active');
+}
